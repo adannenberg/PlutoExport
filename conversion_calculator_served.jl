0c49fd66f918162eb8d653b48f801e474dd752e7
adannenberg <alex.dannenberg@gmail.com>
2024-12-26 09:35:46 -0700

    Update conversion_calculator_served.jl
    updated arg to import statement to try to get right path to myFuncs.jl

--- conversion_calculator_served.jl
+++ conversion_calculator_served.jl
@@ -19,7 +19,7 @@ end
 # ╔═╡ b6aaa574-7002-42dd-b195-765bb88896c6
 begin
 	using Revise, CSV, DataFrames, Dates, Distributions, Format, Grep, HypertextLiteral, Ipopt, JuMP, LinearAlgebra, Loess, NaNStatistics, Optim, Plots, PlutoUI, PrettyTables, Random, Revise, XLSX
-	include("myFuncs.jl")
+	include(string(@__DIR__)*"myFuncs.jl")
 	Print()
 end
 
